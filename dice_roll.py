a1f6b9a3e1a1cbeed2777c4901c1667dd1e6f8be
Hasaam N <nasirhasaam@proton.me>
2026-07-07 18:14:18 -0400

    Update dice_roll.py

---
+++ dice_roll.py
@@ -0,0 +1,14 @@
+import random
+while True:
+    choice = input("Would like you like to roll the dice? (y/n): ").lower()
+    if choice == 'y':
+        die1 = random.randit(1,6)
+        die2 = random.randint(1,6)
+        print(f"({die1}, {die2})")
+    
+    elif choice == 'n':
+        print("Thank you for playing. ")
+        break
+    
+    else:
+        print("Invalid choice! ")
