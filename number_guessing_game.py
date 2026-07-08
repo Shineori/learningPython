3344f897e859459f7456b2fccd5f9d9e03938772
Hasaam N <nasirhasaam@proton.me>
2026-07-08 04:23:19 -0400

    Create number_guessing_game.py

---
+++ number_guessing_game.py
@@ -0,0 +1,18 @@
+# Following a tutotrial by: 'Programming with Mosh' on YT.
+import random
+
+number_to_guess = random.randint(1,100)
+while True:
+    try:
+        guess = int(input("Guess the number between 1 and 100. "))
+
+        if guess < number_to_guess:
+            print('Too low!')
+        elif guess > number_to_guess:
+            print('Too high!')
+        else:
+            print('Congrats! You guessed the number!')
+            break
+
+    except ValueError:
+        print("Please enter in a valid number. ")
